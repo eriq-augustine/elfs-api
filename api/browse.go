0c595c50add5781df0a00d95c7fc11e2739934c3
Eriq Augustine <eriq.augustine@gmail.com>
2017-08-20 16:25:29 -0700

    Now using new functionality of goapi so that we can stream file contents and properly set the content type.

--- api/browse.go
+++ api/browse.go
@@ -1,7 +1,6 @@
 package api;
 
 import (
-   "io"
    "mime"
    "net/http"
    "path"
@@ -19,19 +18,19 @@ import (
    "github.com/eriq-augustine/elfs-api/fsdriver"
 );
 
-func browse(username goapi.UserName, partition string, rawDirentId string, response http.ResponseWriter) (interface{}, int, error) {
+func browse(username goapi.UserName, partition string, rawDirentId string) (interface{}, int, string, error) {
    golog.Debug("Serving: " + partition + "::[" + rawDirentId + "]");
 
    // Get the driver.
    driver, err := fsdriver.GetDriver(partition);
    if (err != nil) {
-      return "", 0, err;
+      return "", 0, "", err;
    }
 
    // Auth the user for this partition.
    userId, err := auth.AuthenticateFilesystemUser(driver, string(username));
    if (err != nil) {
-      return "", 0, err;
+      return "", http.StatusUnauthorized, "", err;
    }
 
    var direntId dirent.Id = dirent.Id(rawDirentId);
@@ -41,20 +40,20 @@ func browse(username goapi.UserName, partition string, rawDirentId string, respo
 
    direntInfo, err := driver.GetDirent(userId, direntId);
    if (err != nil) {
-      return "", 0, err;
+      return "", http.StatusNotFound, "", err;
    }
 
    if (direntInfo.IsFile) {
-      return serveFile(userId, driver, direntInfo, response);
+      return serveFile(userId, driver, direntInfo);
    } else {
       return serveDir(userId, driver, direntInfo);
    }
 }
 
-func serveDir(userId user.Id, driver *driver.Driver, dirInfo *dirent.Dirent) (interface{}, int, error) {
+func serveDir(userId user.Id, driver *driver.Driver, dirInfo *dirent.Dirent) (interface{}, int, string, error) {
    children, err := driver.List(userId, dirInfo.Id);
    if (err != nil) {
-      return "", 0, err;
+      return "", 0, "", err;
    }
 
    var dirents []*model.DirEntry = make([]*model.DirEntry, 0, len(children));
@@ -62,30 +61,14 @@ func serveDir(userId user.Id, driver *driver.Driver, dirInfo *dirent.Dirent) (in
       dirents = append(dirents, model.DirEntryFromDriver(child));
    }
 
-   return messages.NewListDir(dirents), 0, nil;
+   return messages.NewListDir(dirents), 0, "", nil;
 }
 
-// TODO(eriq): goapi needs some work to handle streaming responses.
-// TODO(eriq): Also needs more work with headers. Here we are calling WriteHeaders twice.
-
-func serveFile(userId user.Id, driver *driver.Driver, fileInfo *dirent.Dirent, response http.ResponseWriter) (interface{}, int, error) {
-   // Set the content type before we write anything.
-   response.Header().Set("Content-Type", mime.TypeByExtension(path.Ext(fileInfo.Name)));
-
+func serveFile(userId user.Id, driver *driver.Driver, fileInfo *dirent.Dirent) (interface{}, int, string, error) {
    reader, err := driver.Read(userId, fileInfo.Id);
    if (err != nil) {
-      return "", 0, err;
+      return "", 0, "", err;
    }
 
-   _, err = io.Copy(response, reader);
-   if (err != nil) {
-      return "", 0, err;
-   }
-
-   err = reader.Close();
-   if (err != nil) {
-      return "", 0, err;
-   }
-
-   return "", 0, nil;
+   return reader, 0, mime.TypeByExtension(path.Ext(fileInfo.Name)), nil;
 }
